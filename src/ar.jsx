b01b68a42799734bc4c72df4696a78763fde0f81
SOLOMAN VARGHESE <solomon@americaninference.com>
2025-11-23 19:48:32 +0530

    AR Styling

--- src/ar.jsx
+++ src/ar.jsx
@@ -110,8 +110,8 @@ useEffect(() => {
 
             {/* 3D Viewer Container */}
             <div className="relative bg-gray-50 w-full h-[450px]">
-<model-viewer
-   src="/realistic_burger.glb"
+{/* <model-viewer
+   src="/realistic_burger_fixed.glb"
   camera-controls
   auto-rotate
 
@@ -129,8 +129,34 @@ useEffect(() => {
     className="absolute bottom-6 left-1/2 -translate-x-1/2 bg-orange-600 hover:bg-orange-700 text-white px-6 py-3 rounded-full shadow-lg font-semibold"
   >
     View in AR
-  </button></model-viewer>
-              
+  </button></model-viewer> */}
+              <model-viewer
+  src="/realistic_burger.glb"
+  camera-controls
+  auto-rotate
+
+  ar
+  ar-modes="scene-viewer webxr"
+  ar-placement="floor"
+  ar-scale="fixed"
+
+  environment-image="https://modelviewer.dev/shared-assets/environments/restaurant.hdr"
+  exposure="1.2"
+  shadow-intensity="1"
+  shadow-softness="1"
+  camera-target="0m 0m 0m"
+  camera-orbit="0deg 75deg 2.5m"
+  field-of-view="35deg"
+
+  style="width: 400px; height: 400px; background: lightgray;"
+>
+  <button
+    slot="ar-button"
+    className="absolute bottom-6 left-1/2 -translate-x-1/2 bg-orange-600 hover:bg-orange-700 text-white px-6 py-3 rounded-full shadow-lg font-semibold"
+  >
+    View in AR
+  </button>
+</model-viewer>
               
             </div>
 
